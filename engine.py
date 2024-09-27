04ec4756214367c7527a6edefdacafdf2ffdbedb
Garam Jung <36662849+Nullsp4ce@users.noreply.github.com>
2024-09-27 22:11:08 +0900

    impl outputs...
    - time, nodes, NPS

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 import chess
+from time import perf_counter
+from math import trunc
 
 INFINITY = 32768
 START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
@@ -28,19 +30,28 @@ class Minsikfish:
 
     def awake(self):
         # IDDFS function
+        self.nodes = 0
+        start_time = perf_counter()
         this_depth = 0
         force_depth = 1  # depth-limiting option for example
         # TODO: change into while-true + stop + time management
         while this_depth < force_depth:
             this_depth += 1
             (pv, score) = self.struggle(this_depth)
+            end_time = perf_counter()
+            millis_time = trunc((end_time - start_time) * 1000)
+            nps = trunc(self.nodes / (end_time - start_time))
             pv_uci = list(map(lambda move: move.uci(), pv))
-            print(f"info depth {this_depth} score cp {score} pv {' '.join(pv_uci)}")
+            print(
+                f"info depth {this_depth} score cp {score} time {millis_time} nodes {self.nodes} nps {nps} pv {' '.join(pv_uci)}"
+            )
         return pv_uci[0]
 
     def struggle(self, depth=1) -> tuple[list[chess.Move], int]:
         # actually search function
         # TODO: support partial search
+
+        self.nodes += 1
 
         if self.board.is_checkmate():
             return ([], -INFINITY)
